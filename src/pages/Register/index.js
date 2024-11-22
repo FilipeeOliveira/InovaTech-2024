abc9877c44c33e3fa9c5729df76bae77e880478a
Guilherme Quincó <guilhermeqcorrea@gmail.com>
2024-11-21 22:38:04 -0400

    aplicando estilo na página de post e de perfl, inplementando bottom sheet

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,57 +1,70 @@
-import React, { useState } from 'react';
-import { Alert } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Container, Input, RegisterButton, ButtonText, WelcomeText, ButtonsContainer } from './RegisterStyles';
+import React, { useState } from "react";
+import { Alert } from "react-native";
+import uuid from "react-native-uuid";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {
+  Container,
+  Input,
+  RegisterButton,
+  ButtonText,
+  WelcomeText,
+  ButtonsContainer,
+} from "./RegisterStyles";
 
 function Register({ navigation }) {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleRegister = async () => {
-        if (!username.trim() || !password.trim()) {
-            Alert.alert('Erro', 'Por favor, preencha todos os campos');
-            return;
-        }
+  const handleRegister = async () => {
+    if (!username.trim() || !password.trim()) {
+      Alert.alert("Erro", "Por favor, preencha todos os campos");
+      return;
+    }
 
-        try {
-            const existingUser = await AsyncStorage.getItem(username);
-            if (existingUser !== null) {
-                Alert.alert('Erro', 'Nome de usuário já existe');
-            } else {
-                await AsyncStorage.setItem(username, JSON.stringify({ password }));
-                Alert.alert('Sucesso', 'Usuário registrado com sucesso');
-                navigation.replace('Login');
-            }
-        } catch (error) {
-            console.error('Erro ao salvar dados de registro:', error);
-        }
-    };
+    try {
+      const existingUser = await AsyncStorage.getItem(username);
+      if (existingUser !== null) {
+        Alert.alert("Erro", "Nome de usuário já existe");
+      } else {
+        const userId = uuid.v4();
 
-    return (
-        <Container>
-            <WelcomeText>Crie sua conta</WelcomeText>
+        await AsyncStorage.setItem(
+          username,
+          JSON.stringify({ password, userId })
+        );
+        Alert.alert("Sucesso", "Usuário registrado com sucesso");
+        navigation.replace("Login");
+      }
+    } catch (error) {
+      console.error("Erro ao salvar dados de registro:", error);
+    }
+  };
 
-            <Input
-                placeholder="Digite seu usuário"
-                placeholderTextColor="#FFFFFF"
-                value={username}
-                onChangeText={setUsername}
-            />
-            <Input
-                placeholder="Digite sua senha"
-                placeholderTextColor="#FFFFFF"
-                secureTextEntry={true}
-                value={password}
-                onChangeText={setPassword}
-            />
+  return (
+    <Container>
+      <WelcomeText>Crie sua conta</WelcomeText>
 
-            <ButtonsContainer>
-                <RegisterButton onPress={handleRegister}>
-                    <ButtonText>Registre-se</ButtonText>
-                </RegisterButton>
-            </ButtonsContainer>
-        </Container>
-    );
+      <Input
+        placeholder="Digite seu usuário"
+        placeholderTextColor="#FFFFFF"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <Input
+        placeholder="Digite sua senha"
+        placeholderTextColor="#FFFFFF"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={setPassword}
+      />
+
+      <ButtonsContainer>
+        <RegisterButton onPress={handleRegister}>
+          <ButtonText>Registre-se</ButtonText>
+        </RegisterButton>
+      </ButtonsContainer>
+    </Container>
+  );
 }
 
 export default Register;
--- src/routes.js
+++ src/routes.js
@@ -1,41 +1,59 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faNewspaper, faUser, faMap } from '@fortawesome/free-solid-svg-icons'; // Importe os ícones desejados
-import Posts from './pages/Posts';
-import Profile from './pages/Profile';
-import Map from './pages/Map';
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faNewspaper, faUser, faMap } from "@fortawesome/free-solid-svg-icons"; // Importe os ícones desejados
+import Posts from "./pages/Posts";
+import Profile from "./pages/Profile";
+import Map from "./pages/Map";
 
 const Tab = createBottomTabNavigator();
 
 export function Routes({ userCredentials }) {
-    return (
-        <Tab.Navigator
-            screenOptions={({ route }) => ({
-                tabBarIcon: ({ focused, color, size }) => {
-                    let iconName;
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "#101010",
+        },
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
 
-                    if (route.name === 'Denúncias') {
-                        iconName = faNewspaper; // Defina o ícone para a guia "Denuncias"
-                    } else if (route.name === 'Perfil') {
-                        iconName = faUser; // Defina o ícone para a guia "Profile"
-                    } else if (route.name === 'Mapa') {
-                        iconName = faMap;
-                    }
+          if (route.name === "Denúncias") {
+            iconName = faNewspaper; // Defina o ícone para a guia "Denuncias"
+          } else if (route.name === "Perfil") {
+            iconName = faUser; // Defina o ícone para a guia "Profile"
+          } else if (route.name === "Mapa") {
+            iconName = faMap;
+          }
 
-                    // Retorna o ícone correspondente à guia
-                    return <FontAwesomeIcon icon={iconName} size={size} color={color} />;
-                },
-            })}
-        >
-            <Tab.Screen name="Denúncias" component={Posts} />
-            <Tab.Screen name='Mapa' component={Map} />
-            {userCredentials && (
-                <Tab.Screen name="Perfil">
-                    {props => <Profile {...props} userCredentials={userCredentials} />}
-                </Tab.Screen>
+          // Retorna o ícone correspondente à guia
+          return (
+            <FontAwesomeIcon
+              icon={iconName}
+              size={size}
+              color={focused ? "yellow" : "white"}
+            />
+          );
+        },
+
+        tabBarLabel: false,
+      })}
+    >
+      {userCredentials && (
+        <>
+          <Tab.Screen name="Denúncias">
+            {(props) => <Posts {...props} userCredentials={userCredentials} />}
+          </Tab.Screen>
+          <Tab.Screen name="Mapa" component={Map} />
+          <Tab.Screen name="Perfil">
+            {(props) => (
+              <Profile {...props} userCredentials={userCredentials} />
             )}
-
-        </Tab.Navigator>
-    );
+          </Tab.Screen>
+        </>
+      )}
+    </Tab.Navigator>
+  );
 }
